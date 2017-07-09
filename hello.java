d1fe753d112572b36eb2448b0f7bdfbb0082c9c3
aa7863 <abhijeet_dinesh@srmuniv.edu.in>
2017-07-09 15:46:53 +0530

    hello 5 times

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello 
+{
+    
+public static void main(String args[]) 
+    
+{
+        
+    
+int x;
+   
+ for (x=0; x<5; x++)
+    
+{System.out.println("Hello");}
+    
+}
+
+}
